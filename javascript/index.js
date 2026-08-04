ebcf72bec43e1a3c3b35ea19e99e79adead43ae3
Jmike-dev <gmikekamau@gmail.com>
2026-08-04 11:21:47 +0300

    javascript:began javascript class

---
+++ javascript/index.js
@@ -0,0 +1,21 @@
+// singleline comment
+
+/*
+1. multiline comments
+2. multiline comments
+3. multiline comments
+*/
+
+// declaring variables
+const name = "mike"
+let   names = "mike"
+
+// const name = "karanja"
+
+
+//Data types
+let age = 35; // number
+
+let AverageMarks = 78.9; // float
+const gender = "female"; //string
+
